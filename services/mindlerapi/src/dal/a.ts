6992178ae6ee589bc474cbb9e99b6e5f1e18077f
Simon McEnlly <simon.mcenlly@gmail.com>
2024-05-07 09:21:29 +1000

    Initial commit

---
+++ services/mindlerapi/src/dal/a.ts
@@ -0,0 +1,3 @@
+export default function sampleExport() {
+  return 'dal';
+}
---
+++ test.ts
@@ -0,0 +1,7 @@
+import dal from "@dal/a";
+import test from "@test/a";
+import core from "@core/a";
+
+console.log(dal());
+console.log(test());
+console.log(core());
